128483af5656b37b5676f76f356cf25d548dfef3
Ricky Brown <ricky@xxi.capital>
2021-10-13 20:38:08 -0400

    fix replace

--- assets/js/bd-wizard.js
+++ assets/js/bd-wizard.js
@@ -18,9 +18,8 @@ $(actions).on('change', function(e) {
 });
 
 $('.purpose-radio-input.covid').on('change', function(e) {
-  console.log('clicked!')
   if(e.target.value === 'yes') {
-    window.location.replace = 'https://columbus.applyforhope.com/cols_era_bspc#/'
+    window.location.replace('https://columbus.applyforhope.com/cols_era_bspc#/')
     overlay.style.display = 'block';
   }
   else if(e.target.value === 'no') {
@@ -31,9 +30,9 @@ $('.purpose-radio-input.covid').on('change', function(e) {
 $('.purpose-radio-input.returning').on('change', function(e) {
   overlay.style.display = 'block';
   if(e.target.value === 'yes') {
-    window.location.replace = 'https://cm2compass.neworg.com/Default.asp?PageNum=105'
+    window.location.replace('https://cm2compass.neworg.com/Default.asp?PageNum=105')
   }
   else if(e.target.value === 'no') {
-    window.location.replace = 'https://cm2compass.neworg.com/Default.asp?PageNum=94'
+    window.location.replace('https://cm2compass.neworg.com/Default.asp?PageNum=94')
   }
 });
